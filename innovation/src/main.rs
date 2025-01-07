847331251918b7a5eb1074e615e1fe0e1596155f
Brian T <brianjtascone@gmail.com>
2025-01-07 22:42:03 +0000

    test

--- innovation/src/main.rs
+++ innovation/src/main.rs
@@ -1,3 +1,22 @@
+
+
+
 fn main() {
-    println!("Hello, world!");
+    fn sort<const N: usize>(arr: &mut [i32; N]) -> &[i32; N] {
+        let answer: &mut [i32; N] = arr;
+
+        for int in answer.iter_mut() {
+            *int *= 2;
+        }
+
+        
+
+        return answer;
+    }
+
+    let arr: [i32; 5] = [1, 4, 2, 21, -5];
+    let sorted_arr = sort(&arr);
+
+    println!("\n");
+    println!("{:?}", sorted_arr);
 }
