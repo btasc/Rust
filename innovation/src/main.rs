14392a8d5393827c80f9d99adc7bbae4276dfc6b
Brian-Tas <brianjtascone@gmail.com>
2025-01-08 13:27:58 +0000

    Map

--- innovation/src/main.rs
+++ innovation/src/main.rs
@@ -1,10 +1,37 @@
 extern crate rand;
 use rand::Rng;
 
-struct Test {
-    
+struct Map<K, V> {
+    values: Vec<(K, V)>,
+}
+
+impl<K: PartialEq, V> Map <K, V> {
+    fn new() -> Map<K, V> {
+        Map { values: Vec::new(), }
+    }
+
+    fn insert(&mut self, key: K , value: V) {
+        self.values.push((key, value));
+    }
+
+    fn get(&self, key: K) -> Option<&V> {
+        for i in 0..self.values.len() {
+            if self.values[i].0 == key {
+                return Some(&self.values[i].1);
+            }
+        }
+
+        None
+    }
 }
 
 fn main() { 
-    
+    let mut map = Map::new();
+
+    map.insert("hi", 10);
+
+    let map_answer = map.get("hi").unwrap();
+    let map_times_two = map_answer * 2;
+
+    println!("{:?}", map_times_two);
 }
