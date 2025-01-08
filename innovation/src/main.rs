ebb4ee934fd785c5d15ab693cccf80dda711fb0a
Brian T <brianjtascone@gmail.com>
2025-01-08 01:23:36 +0000

    Test

--- innovation/src/main.rs
+++ innovation/src/main.rs
@@ -1,19 +1,10 @@
-fn fibonacci(fib_len: usize) -> Vec<i32> {
-    assert!(fib_len >= 2, "N must be at least 2");
+extern crate rand;
+use rand::Rng;
 
-    let mut sequence = vec![0, 1];
-
-    for i in 2..fib_len {
-        sequence.push(sequence[i - 1] + sequence[i - 2]);
-    }
-
-    return sequence;
+struct Test {
+    
 }
 
 fn main() { 
-    let fib_len: i32 = 1000;
-    let fib_sequence: Vec<i32> = fibonacci(fib_len as usize);
-
-
-    println!("{:?}", fib_sequence);
+    
 }
---
+++ innovation/target/debug/deps/rand_chacha-18c5476c9bd6deeb.d
@@ -0,0 +1,9 @@
+/workspaces/Rust/innovation/target/debug/deps/librand_chacha-18c5476c9bd6deeb.rmeta: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
+
+/workspaces/Rust/innovation/target/debug/deps/librand_chacha-18c5476c9bd6deeb.rlib: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
+
+/workspaces/Rust/innovation/target/debug/deps/rand_chacha-18c5476c9bd6deeb.d: /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
+
+/home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs:
+/home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs:
+/home/codespace/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs:
