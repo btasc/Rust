d52843811021d68b884d0874b98d691acdb0e9e0
Brian-Tas <brianjtascone@gmail.com>
2025-07-31 19:31:32 -0400

    Removed cway and Simple Projects

--- Simple Projects/cway/target/debug/deps/cway-5fd59acc0f0b4b31.d
+++
@@ -1,5 +0,0 @@
-D:\Dev\Rust\Simple Projects\cway\target\debug\deps\cway-5fd59acc0f0b4b31.d: src\main.rs
-
-D:\Dev\Rust\Simple Projects\cway\target\debug\deps\libcway-5fd59acc0f0b4b31.rmeta: src\main.rs
-
-src\main.rs:
--- Simple Projects/cway/target/debug/deps/cway-950aa52aa313164f.d
+++
@@ -1,5 +0,0 @@
-D:\Dev\Rust\Simple Projects\cway\target\debug\deps\cway-950aa52aa313164f.d: src\main.rs
-
-D:\Dev\Rust\Simple Projects\cway\target\debug\deps\libcway-950aa52aa313164f.rmeta: src\main.rs
-
-src\main.rs:
